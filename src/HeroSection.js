e37ad8b37c6516bac6df7534f8382b4918b72bbf
Ochi Daniel <dtonye338@gmail.com>
2023-02-25 09:34:09 +0100

    final commit

--- src/HeroSection.js
+++ src/HeroSection.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect }  from 'react';
 import NavBar from './NavBar';
 
+
 const HeroSection = () => {
   // const [cityInput, setCityInput] = useState(['Italy'])
   const [capital, setCapital] = useState(false)
   const [id, setId] = useState('')
-  const [idFromButtonClick, setIdFromButtonClick] = useState('')
-  // const [cityFetch, setCityFetch] = useState('')
+  const [idFromButtonClick, setIdFromButtonClick] = useState('Nigeria')
   const [targetCity, setTargetCity] = useState([{
     id: 1,
     city: 'Manchester'
@@ -22,20 +22,33 @@ const HeroSection = () => {
 {
     id: 4,
     city: 'London'
+},
+{
+  id: 5,
+  city: 'Washington'
 }
 ])
 
+//BUTTON CLick
   const handleClick = (e) =>{
     e.preventDefault();
-    console.log("Button Active")
+if(id === null || id === ''){
+  alert('Please enter a city')
+}
+   else{
+      setIdFromButtonClick(id)
+      setId('')
+    }
+     
+  }
+//CLICKED CITIES
+  const handleTarget = (id) =>{
+    console.log(id)
     setIdFromButtonClick(id)
   }
-  // const handleTarget = (e) =>{
-  //   console.log("Target Active")
-  //   setCityFetch(targetCity)
-  // }
 
   const fetchData = async () => {
+  
       try{
           const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=5993123942ec4d87b45144923221706&&q=${idFromButtonClick}`);
           const json = await response.json()
@@ -46,29 +59,54 @@ const HeroSection = () => {
       }
   }
   useEffect(() => {
+
       fetchData();
   },[idFromButtonClick])
 
- console.log(capital)
+console.log(capital)
+const code = capital.current?.condition.code
+//WEATHER ICON
+const iconImg = {
+          src: capital.current?.condition.icon.substr('//cdn.weatherapi.com/weather/64x64/day/113.png'),
+          alt: 'icon'
+}
+
   return (
     <div>
-        <section className='hero relative'>
+        <section className={`hero relative 
+                      ${ code === 1003|| code == 1072 ||
+                        code == 1069 ||
+                        code == 1153 ||
+                        code == 1180 ||
+                        code == 1183 ||
+                        code == 1186 ||
+                        code == 1189 ||
+                        code == 1192 ||
+                        code ==  1195||
+                        code == 1204 ||
+                        code == 1207 ||
+                        code == 1240 ||
+                        code == 1243 ||
+                        code == 1246 ||
+                        code == 1249 ||
+                        code == 1252 ? 'overcast' : 'hero' }`}>
             <div className='container mx-auto flex flex-col space-y-20 md:space-y-96 px-6 md:px-0'>
                   <span className='text-sm pt-10 font-bold text-white'>the.weather</span>
-                          <div className='flex space-x-8 items-center content'>
-                      <h1 className='text-white text-6xl md:text-8xl'>{capital.current?.temp_c}&#176;</h1>
-                   <div className='flex flex-col space-y-2'>
-                        <h2 className='text-white text-3xl md:text-5xl font-light'>{capital.location?.name}</h2>
+
+                          <div className='flex space-x-8 md:space-x-10 items-center md:max-w-xl content'>
+                      <h1 className='text-white text-6xl md:text-8xl md:w-1/3'>{capital.current?.temp_c}&#176;</h1>
+                   <div className='flex flex-col items-center py-2 px-0 md:px-0 md:py-0 text-center space-y-2 md:w-1/3'>
+                        <h2 className='text-white text-3xl md:text-4xl font-light'>{capital.location?.name}</h2>
                         <p className='text-white text-xs md:text-md'>{capital.location?.localtime}</p>
                       </div>
-                        <div className='flex flex-col space-x-2'>
-                          {/* <img src={} alt="icon" /> */}
-                          <div className='text-white text-center text-5xl'>&#176;</div>
-                          <span className='text-white text-sm md:text-md'>{capital.current?.condition.text}</span>
+                        <div className='flex flex-col items-center text-center md:justify-center md:w-1/3'>
+                          <img src={iconImg.src} class="-mt-4" alt={iconImg.alt} />
+                          {/* <div className='text-white text-5xl'></div> */}
+                          <div className='text-white text-sm md:text-md'>{capital.current?.condition.text}</div>
                         </div>
                     </div>
               </div>
-              <NavBar setId={setId} handleClick={handleClick} targetCity={targetCity} id={id} />
+              <NavBar setId={setId} handleClick={handleClick} targetCity={targetCity} handleTarget={handleTarget} details={capital} id={id} />
         </section>
     </div>
   )
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,37 +1,36 @@
 import React from 'react'
 
-
-const NavBar = ({ setId, handleClick, id, targetCity}) => {
+const NavBar = ({ setId, handleClick, handleTarget, id, targetCity, details}) => {
 
 
   return (
     <div>
         <div className='navbar'>
             <form onSubmit={handleClick}> 
-            <input type='text' className='search-box relative' value={id} placeholder='Enter City...' onChange={e => setId(e.target.value)}/>
+            <input type='text' className='search-box relative text-white' value={id} placeholder='Search location...' onChange={e => setId(e.target.value)}/>
             <button type='submit' className='p-2 text-sm text-white bg-black rounded-md absolute top-10 right-15'>Search</button>
             </form>
             <div className='flex flex-col space-y-6 pt-8 pb-8 cities'>
                 {
                   targetCity.map(cities  => (
-                  <ul key={cities.id} className='text-white  pl-4 text-sm'>{cities.city}</ul>
+                  <ul key={cities.id} onClick={() => handleTarget(cities.city)} className='w-20 text-white pl-4 text-sm'>{cities.city}</ul>
                   ))
                 }
                 </div>
 
-            <div className='flex flex-col space-y-6 pt-8 pb-8 cities'>
-                <div className='text-white pl-4 text-sm'>Manchester</div>
-                <div className='text-white pl-4 text-sm'>New York</div>
-                <div className='text-white pl-4 text-sm'>Birmingham</div>
-                <div className='text-white pl-4 text-sm'>Nigeria</div>
+            <div className='flex flex-col space-y-6 pt-8 pb-8 cities font-light'>
+            <div className='text-white pl-4 font-bold text-sm'>Weather Details</div>
+                <div className='text-white pl-4 text-sm'>{`Humidity : ${details.current?.humidity}%`}</div>
+                <div className='text-white pl-4 text-sm'>{`Wind kp/h : ${details.current?.wind_kph}%`}</div>
+                <div className='text-white pl-4 text-sm'>{`Pressure : ${details.current?.pressure_in}%`}</div>
             </div>
 
-            <div className='flex flex-col space-y-6 pt-8 pb-8 cities'>
-                <div className='text-white pl-4 text-sm'>Manchester</div>
-                <div className='text-white pl-4 text-sm'>New York</div>
-                <div className='text-white pl-4 text-sm'>Birmingham</div>
-                <div className='text-white pl-4 text-sm'>Nigeria</div>
-            </div>
+            <div className='flex flex-col space-y-6 pt-8 pb-8 cities mb-0'>
+                <div className='text-white pl-4 font-bold text-sm'>Area Details</div>
+                <div className='text-white pl-4 text-sm'>{`Country - ${details.location?.country}`}</div>
+                <div className='text-white pl-4 text-sm'>{`Region - ${details.location?.region}`}</div>
+              <div className='text-white pl-4 pb-0 text-center text-sm pt-20'>DESIGNED BY OCHI DANIEL</div>
+        </div>
             
         </div>
     </div>
